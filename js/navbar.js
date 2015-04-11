7a87045832c7713d920c339964922a73fc243965
Florian Kostrikin <Hermes>
2015-04-11 00:27:23 -0500

    changed the color of the logo, changed the video presentation div, added some sassy icons, changed the navbar links to support the one day ready IFG

---
+++ js/navbar.js
@@ -0,0 +1,21 @@
+document.getElementById("nav01").innerHTML=
+    "<div class='container-fluid'>" +
+            "<div class='navbar-header'>" +
+                "<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>" +
+                    "<span class='icon-bar'></span>"+
+                    "<span class='icon-bar'></span>"+
+                    "<span class='icon-bar'></span>"+
+                  "</button>"+
+                "<a class='navbar-brand' href='#'>InglesFacil</a>"+
+            "</div>"+
+                "<div class='collapse navbar-collapse' id='myNavbar'>"+
+                    "<ul class='nav navbar-nav'>"+
+                        "<li class="active"><a href='#header'>Inicio</a></li>" +
+                        "<li><a href='#testimonios_div'>Testimonios</a></li>" +
+                        "<li><a href="'suscripcion_div'>Suscribete</a></li>" +
+                    "</ul>"+
+                    "<ul class='nav navbar-nav navbar-right'>"+
+                        "<li><a href='#'>Sobre nosotros</a></li>" +
+                    "</ul>"+
+                "</div>"+
+        "</div>"
